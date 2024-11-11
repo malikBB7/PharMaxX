d3692ef780ed84d8ae79233c13679021c9426174
Shaik Malikbaba <malikbabask@gmail.com>
2024-11-11 10:37:52 +0530

    Commit message describing changes

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,13 @@
-// src/utils/auth.js
-
 export const getAccessToken = async () => {
-    const response = await fetch('https://pharmaxx.powerappsportals.com/get-access-token', {
+    const response = await fetch('http://localhost:5000/get-access-token', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            clientId: '41f46ae1-1e05-4e30-870d-5e684c8ea7e2',
-            tenantId: 'e4da8020-3c09-40ab-8fa0-1707bc163563',
-            clientSecret: 'SIN8Q~iOmz3lV_loTO3Y6Z3h4kjyWcA~pSEcbbRJ',
+            clientId: process.env.REACT_APP_CLIENT_ID, // Use environment variables
+            tenantId: process.env.REACT_APP_TENANT_ID,
+            clientSecret: process.env.REACT_APP_CLIENT_SECRET,
         }),
     });
 
